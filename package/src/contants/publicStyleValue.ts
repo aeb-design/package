f72dce2ad0c5a9cbecaf38742392dbc8325360ed
liuyuyang <2281487673@qq.com>
2024-07-01 02:29:20 +0800

    feat: 添加公用主题色

---
+++ package/src/contants/publicStyleValue.ts
@@ -0,0 +1,79 @@
+export const primaryColor_1 = 'rgb(235, 241, 255)';
+
+export const primaryColor_2 = 'rgb(207, 221, 255)';
+
+export const primaryColor_3 = 'rgb(161, 191, 255)';
+
+export const primaryColor_4 = '#699bff';
+
+export const primaryColor_5 = 'rgb(54, 124, 255)';
+
+export const primaryColor_6 = 'rgb(3, 95, 255)';
+
+export const primaryColor_7 = '#0053d9';
+
+export const primaryColor_8 = '#0047b3';
+
+export const primaryColor_9 = '#003a8c';
+
+export const primaryColor_10 = '#002c66';
+
+export const errorColor_1 = "#fdecee";
+
+export const ertorColor_2 = "#f9d7d9";
+
+export const errorColor_3 = "#f8b9be";
+
+export const errorColor_4 = "#f78d94";
+
+export const errorColor_5 = "#f36d78";
+
+export const errorColor_6 = "#e62c4b";
+
+export const errorColor_7 = "#bf1b33";
+
+export const errorColor_8 = "#990e25";
+
+export const errorColor_9 = "#730519";
+
+export const errorColor_10 = "#4d000f";
+
+export const warningColor_1 = "#fff3e0";
+
+export const warningColor_2 = "#ffebcc";
+
+export const warningColor_3 = "#ffd599";
+
+export const warningColor_4 = "#ffbd66";
+
+export const warningColor_5 = "#ffa333";
+
+export const warningColor_6 = "#f58300";
+
+export const warningColor_7 = "#cf6b00";
+
+export const warningColor_8 = "#a85400";
+
+export const warningColor_9 = "#823f00";
+
+export const warningColor_10 = "#5c2b00";
+
+export const successColor_1 = "#e6fff5";
+
+export const successColor_2 = "#ccffeb";
+
+export const successColor_3 = "#91f2ca";
+
+export const successColor_4 = "#5ce6aa";
+
+export const successColor_5 = "#2bd98b";
+
+export const successColor_6 = "#00cc6d";
+
+export const successColor_7 = "#00a656";
+
+export const successColor_8 = "#008040";
+
+export const successColor_9 = "#00592b";
+
+export const successColor_10 = "#003318";
